d6e2712978331c1589f9b8ec1b9ce2a0f05880a8
Danyl Bielytskyi <paintein666@gmail.com>
2024-06-24 12:16:54 +0300

    Create Profile layout

--- src/components/Main/Profile/Profile.tsx
+++ src/components/Main/Profile/Profile.tsx
@@ -1,10 +1,24 @@
-
+import s from "./Profile.module.css"
 
 function Profile() {
     return(
-        <div>
-            <div>
-                <img src="https://sotni.ru/wp-content/uploads/2023/08/mett-molloi-11.webp" alt="wallpaper"/>
+        <div className={s.profileWrapper}>
+            <div className={s.wallpaper}>
+                <img
+                    src="https://interier-foto.ru/wp-content/uploads/dlinnye-foto-4.jpg"
+                    alt="wallpaper"
+                />
+            </div>
+            <div className={s.profile}>
+                <img
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSCV3qXCZ7YaJ4MOkCaw17CjrusyoQMp4fFNA&s"
+                    alt="Avatar"
+                />
+                    <span>Ricardo Milos</span>
+            </div>
+            <div className={s.posts}>
+                <div>Is it really u legend?</div>
+                <div>Your flex is amazing</div>
             </div>
         </div>
     )
