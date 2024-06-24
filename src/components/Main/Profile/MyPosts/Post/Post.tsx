182427666d30eb226f182bef3065eb8c4d420a31
Danyl Bielytskyi <paintein666@gmail.com>
2024-06-24 14:35:58 +0300

    Add user nickname and margin between posts

--- src/components/Main/Profile/MyPosts/Post/Post.tsx
+++ src/components/Main/Profile/MyPosts/Post/Post.tsx
@@ -10,13 +10,11 @@ interface postInfo {
 
 function Post(props: postInfo) {
     return (
-        <div className={s.posts}>
-            <div className={s.item}>
-                <img src={props.user.avatarURL} alt="avatar"/>
-                <div className={s.text}>
-                    <span><strong>{props.user.NickName}</strong></span>
-                    <span>{props.massage}</span>
-                </div>
+        <div className={s.item}>
+            <img src={props.user.avatarURL} alt="avatar"/>
+            <div className={s.text}>
+                <span><strong>{props.user.NickName}</strong></span>
+                <span>{props.massage}</span>
             </div>
         </div>
     )
