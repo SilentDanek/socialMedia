f0c6858ef42a5661e7faf147220b2609c43a1774
Danyl Bielytskyi <paintein666@gmail.com>
2024-06-26 11:47:28 +0300

    Add function massagesElements and dialogsElements

--- src/components/Main/Dialogs/Dialogs.tsx
+++ src/components/Main/Dialogs/Dialogs.tsx
@@ -2,20 +2,29 @@ import s from "./Dialogs.module.css"
 import DialogItem from "./DialogItem/DialogItem";
 import Massage from "./Massage/Massage";
 
+function dialogsElements(tempStoreUsers:any) {
+    return tempStoreUsers.map(({name, id}:{name:string, id:number})=> {
+        return <DialogItem name={name} id={id}/>;
+    });
+}
+
+function massagesElements(tempStoreMes:string[]) {
+    return tempStoreMes.map((massage) => <Massage massage={massage}/>);
+
+}
+
 function Dialogs(props:any){
+    const tempStoreUsers = [{name:"Ярик", id:1}, {name:"Саша", id:2}, {name:"Славик", id:3}, {name:"Миша", id:4}];
+    const tempStoreMes = ["Hi", "Hello", "Привет", "Хай"];
+
+
     return(
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name={"Ярик"}   id={1}/>
-                <DialogItem name={"Саша"}   id={2}/>
-                <DialogItem name={"Славик"} id={3}/>
-                <DialogItem name={"Миша"}   id={4}/>
+                {dialogsElements(tempStoreUsers)}
             </div>
             <div className={s.massages}>
-                <Massage massage={"Hi"}/>
-                <Massage massage={"Hello"}/>
-                <Massage massage={"Привет"}/>
-                <Massage massage={"Хай"}/>
+                {massagesElements(tempStoreMes)}
             </div>
         </div>
     )
