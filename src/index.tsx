b950cb74ac837c11cb76873fc153875d016d3274
Danyl Bielytskyi <paintein666@gmail.com>
2024-07-09 14:37:46 +0300

    Remove re-render function, "connect" renders components properly

--- src/index.tsx
+++ src/index.tsx
@@ -10,30 +10,15 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
-function rerenderEntireTree(store:any){
-    root.render(
-        <React.StrictMode>
-            <BrowserRouter>
-                <Provider store = {store}>
-                    <App/>
-                </Provider>
-            </BrowserRouter>
-        </React.StrictMode>
-    );
-}
-
-store.subscribe(() => rerenderEntireTree(store))
-
-rerenderEntireTree(store);
-
-
-
-
-
-
-
-
-
+root.render(
+    <React.StrictMode>
+        <BrowserRouter>
+            <Provider store={store}>
+                <App/>
+            </Provider>
+        </BrowserRouter>
+    </React.StrictMode>
+);
 
 
 // If you want to start measuring performance in your app, pass a function
