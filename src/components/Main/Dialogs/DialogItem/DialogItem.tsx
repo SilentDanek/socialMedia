c915d626c65262c79126429b0eb196b57eaf92e6
Danyl Bielytskyi <paintein666@gmail.com>
2024-10-10 01:38:29 +0300

    feat: added message list and initial chat layout

--- src/components/Main/Dialogs/DialogItem/DialogItem.tsx
+++ src/components/Main/Dialogs/DialogItem/DialogItem.tsx
@@ -1,17 +1,90 @@
-import {NavLink} from "react-router-dom";
-import s from "./DialogItem.module.css"
-import {FC} from "react";
+import { Dispatch, FC, SetStateAction } from 'react';
+import { Photos } from '../../../../redux';
+import { Avatar, Box, Card, CardContent, Chip, Stack, Typography, useTheme } from '@mui/material';
+import unknownUserSvg from '../../../../assets/images/unknown-user.svg';
 
-type User = {
+type DialogItemProps = {
+    hasNewMessages: boolean;
     id: number;
-    name: string;
-};
-export const DialogItem: FC<User> = ({id, name}) => {
-    return (
-        <div className={s.dialog}>
-            <NavLink to={"/dialogs/" + id}>
-                {name}
-            </NavLink>
-        </div>
+    lastDialogActivityDate: string;
+    lastUserActivityDate: string;
+    newMessagesCount: number;
+    photos: Photos;
+    userName: string;
+    setSelectedUser: Dispatch<SetStateAction<number | null>>;
+}
+
+export const DialogItem: FC<DialogItemProps> = ({
+                                                    id,
+                                                    photos,
+                                                    userName,
+                                                    lastDialogActivityDate,
+                                                    lastUserActivityDate,
+                                                    newMessagesCount,
+                                                    hasNewMessages,
+                                                    setSelectedUser
+                                                }) => {
+    const lastMessageData = formatDate(lastDialogActivityDate);
+    const lastUserData = formatDate(lastUserActivityDate);
+
+    const theme = useTheme();
+
+    return (<Card sx={{
+            cursor: 'pointer',
+            transition: 'background-color 0.4s',
+            borderRadius: '12px',
+            '&:hover': {
+                backgroundColor: theme.palette.mode === 'light' ? '#e8e8e8' : '#333333'
+            }
+        }}>
+            <CardContent style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}
+                         onClick={() => setSelectedUser(id)}>
+                <Avatar alt={userName} src={photos.large || unknownUserSvg} sx={{ width: 75, height: 75 }} />
+                <Box sx={{ flexGrow: 1, paddingLeft: '16px' }}>
+                    <Stack direction={'row'} justifyContent={'space-between'}>
+                        <Typography variant="body1" overflow={'clip'}>{userName}</Typography>
+                        <Typography variant="subtitle2" color="textSecondary">
+                            {lastMessageData}
+                        </Typography>
+                    </Stack>
+                    <Stack direction={'row'} justifyContent="space-between" alignItems="center">
+                        <Typography variant="subtitle2" color="textSecondary">
+                            {lastUserData}
+                        </Typography>
+                        {hasNewMessages && <Chip label={newMessagesCount} color="primary" />}
+                    </Stack>
+                </Box>
+            </CardContent>
+        </Card>
     );
 };
+
+
+function formatDate(dateString: string, locale: string = 'uk-UA'): string {
+    const date = new Date(dateString);
+    const now = new Date();
+    const diffMs = now.getTime() - date.getTime();
+    const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
+
+    if (diffDays < 1) {
+        const hours = date.getHours().toString().padStart(2, '0');
+        const minutes = date.getMinutes().toString().padStart(2, '0');
+        return `${hours}:${minutes}`;
+    }
+
+    if (diffDays < 7) {
+        const shortDaysOfWeek = locale === 'uk-UA'
+            ? ['Нд', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб']
+            : ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+        return shortDaysOfWeek[date.getDay()];
+    }
+
+    if (diffDays < 365) {
+        const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long' };
+        return date.toLocaleDateString(locale, options);
+    }
+
+
+    const options: Intl.DateTimeFormatOptions = { day: 'numeric', month: 'long', year: 'numeric' };
+    return date.toLocaleDateString(locale, options);
+}
--- src/components/Main/Dialogs/Dialogs.tsx
+++ src/components/Main/Dialogs/Dialogs.tsx
@@ -1,44 +1,52 @@
-import s from "./Dialogs.module.css";
-import { DialogItem } from "./DialogItem/DialogItem";
-import { Message } from "./Message/Message";
-import { MessageForm } from "./MessageForm/MessageForm";
-import { FC } from "react";
-import { getDialogsPage, useAppSelector } from "../../../redux";
-
-type Users = {
-    id: number;
-    name: string;
-};
-const dialogsElements = (users: Users[]) => {
-    return users.map(({name, id}) => <DialogItem key={id} name={name} id={id}/>);
-};
-
-type Messages = {
-    id: number;
-    message: string;
-};
-const messagesElements = (messages: Messages[]) => {
-    return messages.map(({message, id}) => <Message key={id} message={message}/>);
-};
+import { FC, useState } from 'react';
+import { useGetDialogsQuery, useGetMessagesQuery, useSendMessageMutation } from '../../../api/dialogsAPI.ts';
+import { Preloader, ThemeBox } from '../../common';
+import { Stack } from '@mui/material';
+import { DialogItem } from './DialogItem/DialogItem.tsx';
+import { AddNewMessageForm } from '../../common/AddNewMessageForm/AddNewMessageForm.tsx';
+import { getAuthUserId, useAppSelector } from '../../../redux';
 
 
 const Dialogs: FC = () => {
-
-    const dialogsPage = useAppSelector(getDialogsPage);
+    const { data: recentDialogs, isLoading } = useGetDialogsQuery();
+    const [selectedUser, setSelectedUser] = useState<null | number>(null);
+    console.log(selectedUser);
+    if (isLoading) {
+        return <Preloader />;
+    }
 
     return (
-        <div className={s.dialogs}>
-            <div className={s.dialogsItems}>
-                {dialogsElements(dialogsPage.dialogs)}
-            </div>
-            <div className={s.messages}>
-                <div>
-                    {messagesElements(dialogsPage.messages)}
-                </div>
-            </div>
-            <MessageForm/>
-        </div>
-    )
+        <Stack direction={'row'} sx={{ height: '100%' }}>
+            <ThemeBox sx={{ flexBasis: '400px', height: '100%' }}>
+                {recentDialogs?.map((d) => <DialogItem setSelectedUser={setSelectedUser} key={d.id} {...d} />)}
+            </ThemeBox>
+            {
+                selectedUser && <A selectedUser={selectedUser}/>
+            }
+        </Stack>
+    );
+};
+
+const A = ({ selectedUser }: any) => {
+
+    const userId = useAppSelector(getAuthUserId);
+    const { data: messages, isSuccess } = useGetMessagesQuery({ userId: selectedUser, count:10 });
+    const [sendMessage, {isSuccess: isMessageSent}] = useSendMessageMutation();
+    console.log(messages);
+
+    const handleSendMessage = (newMessage:string) => {
+        sendMessage({body:newMessage, userId:userId || -1});
+    }
+
+    return (<ThemeBox sx={{ flex: 1, height: '100%' }}>
+        {
+            isSuccess &&
+                <>
+                    <AddNewMessageForm blockSubmit={!isMessageSent} sendMessage={handleSendMessage}/>
+                </>
+        }
+        </ThemeBox>
+    );
 };
 
 export default Dialogs;
